588fbd1323ed239d4b779574a1d42976e0de35a0
SomeBodyLite <danhik.maksamov@yandex.ru>
2025-03-02 05:04:13 +0700

    Update usersTemplate.js

--- assets/js/data/usersTemplate.js
+++ assets/js/data/usersTemplate.js
@@ -7,8 +7,8 @@ const usersItemTemplate = (item) => {
             <td><div class="item">${item.date}</div></td>
             <td>
               <div class="item actions">
-                <button class="action-btn edit-btn" data-id="${item.login}" title="Редактировать"><svg class="icon"><use xlink:href="./assets/images/icons.svg#edit"></use></svg></button>
-                <button class="action-btn delete-btn" data-id="${item.login}" title="Удалить"><svg class="icon"><use xlink:href="./assets/images/icons.svg#delete"></use></svg></button>
+                <button class="action-btn edit-btn" data-id="${item.login}" title="Редактировать"><svg class="icon hover"><use xlink:href="./assets/images/icons.svg#edit"></use></svg></button>
+                <button class="action-btn delete-btn" data-id="${item.login}" title="Удалить"><svg class="icon hover"><use xlink:href="./assets/images/icons.svg#delete"></use></svg></button>
               </div>
             </td>
           </tr>`;
