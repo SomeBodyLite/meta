c3643bb2fd739edf287850f4bd3e5c0e964919e5
SomeBodyLite <danhik.maksamov@yandex.ru>
2025-03-20 00:50:12 +0700

    v0.5.5a

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -7,17 +7,64 @@ buttons.forEach(button => {
   let timeoutId;
   let timer = 0;
 
+  const isMobile = () => window.innerWidth <= 768; 
+
+  const showTooltip = (event) => {
+    let x = event.clientX || event.touches[0].clientX;
+    let y = event.clientY || event.touches[0].clientY;
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    const tooltipWidth = tooltip.offsetWidth;
+    const tooltipHeight = tooltip.offsetHeight;
+
+    
+    if (isMobile()) {
+      x = x - tooltipWidth - 10; 
+    } else {
+      x = x + 10; 
+    }
+
+    
+    if (x < 10) {
+      x = 10; 
+    } else if (x + tooltipWidth > screenWidth) {
+      x = screenWidth - tooltipWidth - 10; 
+    }
+
+    if (y + tooltipHeight + 10 > screenHeight) {
+      y = screenHeight - tooltipHeight - 10; 
+    }
+
+    tooltip.style.left = `${x}px`;
+    tooltip.style.top = `${y + 10}px`;
+
+    tooltip.style.visibility = 'visible';
+    tooltip.style.opacity = '1';
+  };
+
   button.addEventListener('mousemove', (event) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => {
-      tooltip.style.visibility = 'visible';
-      tooltip.style.opacity = '1';
-      tooltip.style.left = `${event.clientX + 10}px`; 
-      tooltip.style.top = `${event.clientY + 10}px`;
+      showTooltip(event);
     }, timer); 
   });
 
   button.addEventListener('mouseleave', () => {
+    clearTimeout(timeoutId); 
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.opacity = '0';
+  });
+
+  button.addEventListener('touchstart', (event) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      showTooltip(event);
+    }, timer);
+  });
+
+  button.addEventListener('touchend', () => {
     clearTimeout(timeoutId); 
     tooltip.style.visibility = 'hidden';
     tooltip.style.opacity = '0';
